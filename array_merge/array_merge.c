326ffd3175808c34133390a4632b945a150c4bba
Jack Mahoney <mahon480@morris.umn.edu>
2024-10-03 11:18:00 -0500

    Created merge sort and all tests pass and their are no memory leaks detected.
    Co-authored-by: Ely Johnson <elyjohnson6@gmail.com>

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -3,5 +3,6 @@
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
   // This is obviously broken. It has the right type, though, eh?
+
   return sizes;
 }
--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,12 +1,47 @@
 #include "mergesort.h"
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool needsSorting(int rangeSize) {
+    return rangeSize >= 2;
+}
+
+void mergeRanges(int values[], int startIndex, int midPoint, int endIndex) {
+    const int rangeSize = endIndex - startIndex;
+    int *destination = (int *)malloc(rangeSize * sizeof(int));
+    int firstIndex = startIndex;
+    int secondIndex = midPoint;
+    int copyIndex = 0;
+    while (firstIndex < midPoint && secondIndex < endIndex) {
+        if (values[firstIndex] < values[secondIndex]) {
+            destination[copyIndex] = values[firstIndex++];
+        } else {
+            destination[copyIndex] = values[secondIndex++];
+        }
+        copyIndex++;
+    }
+    while (firstIndex < midPoint) {
+        destination[copyIndex++] = values[firstIndex++];
+    }
+        while (secondIndex < endIndex) {
+        destination[copyIndex++] = values[secondIndex++];
+    }
+    for (int i = 0; i < rangeSize; i++) {
+        values[i + startIndex] = destination[i];
+    }
+    free(destination);
+}
+
+void mergesortRange(int values[], int startIndex, int endIndex) {
+    int rangeSize = endIndex - startIndex;
+    if (needsSorting(rangeSize)) {
+        int midpoint = (startIndex + endIndex) / 2;
+        mergesortRange(values, startIndex, midpoint);
+        mergesortRange(values, midpoint, endIndex);
+        mergeRanges(values, startIndex, midpoint, endIndex);
+    }
+}
 
 void mergesort(int size, int values[]) {
-  // This obviously doesn't actually do any *sorting*, so there's
-  // certainly work still to be done.
-  //
-  // Remember that a key goal here is to learn to use
-  // `malloc/calloc` and `free`, so make sure you explicitly
-  // allocate any new arrays that you need, even if you
-  // might not strictly need to.
-  return;
+    mergesortRange(values, 0, size);
 }
